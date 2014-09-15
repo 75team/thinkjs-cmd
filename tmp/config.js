7e439496d0c1b38a1c6e48e92f25438349d5c9bf
welefen <welefen@gmail.com>
2014-09-15 14:41:52 +0800

    配置里去掉app_group_list

--- tmp/config.js
+++ tmp/config.js
@@ -1,8 +1,6 @@
 module.exports = {
   //配置项: 配置值
   port: 8360, //监听的端口
-  app_group_list: ['Home', 'Admin'], //分组列表
-
   db_type: 'mysql', // 数据库类型
   db_host: '127.0.0.1', // 服务器地址
   db_port: '', // 端口
